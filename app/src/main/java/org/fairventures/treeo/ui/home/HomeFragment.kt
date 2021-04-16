57807e6f49c75cf0d7ab6d5f5b930f52fb56e148
Ivan Paul <paulivan568@gmail.com>
2021-04-16 12:37:34 +0300

    Add Camera X Preview and Image Capture Usecase

--- app/src/main/java/org/fairventures/treeo/ui/home/HomeFragment.kt
+++ app/src/main/java/org/fairventures/treeo/ui/home/HomeFragment.kt
@@ -1,25 +1,30 @@
 package org.fairventures.treeo.ui.home
 
+import android.Manifest
 import android.app.Activity
 import android.app.ActivityManager
 import android.content.Context
 import android.content.Context.ACTIVITY_SERVICE
 import android.content.Context.CAMERA_SERVICE
-import android.content.ContextWrapper
 import android.content.Intent
 import android.content.SharedPreferences
-import android.graphics.Bitmap
-import android.graphics.BitmapFactory
+import android.content.pm.PackageManager
 import android.hardware.SensorManager
 import android.hardware.camera2.CameraManager
+import android.net.Uri
 import android.os.Bundle
-import android.os.Environment
-import android.provider.MediaStore
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.content.FileProvider
+import android.widget.Toast
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.ImageCaptureException
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
@@ -36,13 +41,15 @@ import kotlinx.coroutines.launch
 import org.fairventures.treeo.R
 import org.fairventures.treeo.ui.authentication.LoginLogoutUserViewModel
 import org.fairventures.treeo.util.DeviceInfoUtils
-import org.fairventures.treeo.util.ExifUtil
-import org.fairventures.treeo.util.FILE_NAME
 import org.fairventures.treeo.util.IDispatcherProvider
 import java.io.File
-import java.io.FileOutputStream
-import java.io.IOException
+import java.text.SimpleDateFormat
+import java.util.*
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
 import javax.inject.Inject
+
+typealias LumaListener = (luma: Double) -> Unit
 
 @ExperimentalCoroutinesApi
 @AndroidEntryPoint
@@ -65,9 +72,29 @@ class HomeFragment : Fragment() {
 
     private lateinit var photoFile: File
 
+    private var imageCapture: ImageCapture? = null
+
+    private lateinit var outputDirectory: File
+    private lateinit var cameraExecutor: ExecutorService
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         FirebaseCrashlytics.getInstance().setUserId(getUserId().toString())
+
+        // Request camera permissions
+        if (allPermissionsGranted()) {
+            Toast.makeText(requireContext(),"Permissions Granted",Toast.LENGTH_LONG).show()
+            startCamera()
+        } else {
+            ActivityCompat.requestPermissions(
+                requireActivity(), REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS)
+        }
+
+        outputDirectory = getOutputDirectory()
+
+        cameraExecutor = Executors.newSingleThreadExecutor()
+
+
     }
 
 
@@ -85,27 +112,93 @@ class HomeFragment : Fragment() {
 
         getDeviceInformation()
 
+        setupUI()
+
+        setObservers()
+        simulateCrash()
+    }
+
+    private fun setupUI() {
         logout_button.setOnClickListener {
             logoutUser()
         }
 
         take_picture_button.setOnClickListener {
-            val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-            photoFile = getPhotoFile(FILE_NAME)
-
-            val fileProvider = FileProvider
-                .getUriForFile(requireContext(), "org.fairventures.treeo.fileprovider", photoFile)
-            takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, fileProvider)
-            if (takePictureIntent.resolveActivity(requireActivity().packageManager) != null) {
-                startActivityForResult(takePictureIntent, 1)
-
-            } else {
-                Log.d("Camera Error", "Could not open Camera")
-            }
+            takePhoto()
         }
+    }
 
-        setObservers()
-        simulateCrash()
+    private fun takePhoto() {
+        // Get a stable reference of the modifiable image capture use case
+        val imageCapture = imageCapture ?: return
+
+        // Create time-stamped output file to hold the image
+        val photoFile = File(
+            outputDirectory,
+            SimpleDateFormat(FILENAME_FORMAT, Locale.US
+            ).format(System.currentTimeMillis()) + "||" +getUserId().toString()+ ".jpg")
+
+        // Create output options object which contains file + metadata
+        val outputOptions = ImageCapture.OutputFileOptions.Builder(photoFile).build()
+
+        // Set up image capture listener, which is triggered after photo has
+        // been taken
+        imageCapture.takePicture(
+            outputOptions, ContextCompat.getMainExecutor(requireContext()), object : ImageCapture.OnImageSavedCallback {
+                override fun onError(exc: ImageCaptureException) {
+                    Log.e(TAG, "Photo capture failed: ${exc.message}", exc)
+                }
+
+                override fun onImageSaved(output: ImageCapture.OutputFileResults) {
+                    val savedUri = Uri.fromFile(photoFile)
+                    val msg = "Photo capture succeeded: $savedUri"
+                    Toast.makeText(requireContext(), msg, Toast.LENGTH_LONG).show()
+                    Log.d(TAG, msg)
+                }
+            })
+    }
+
+    private fun startCamera() {
+        val cameraProviderFuture =
+            ProcessCameraProvider.getInstance(requireContext())
+
+        cameraProviderFuture.addListener(Runnable {
+            // Used to bind the lifecycle of cameras to the lifecycle owner
+            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+
+            // Preview
+            val preview = Preview.Builder()
+                .build()
+                .also {
+                    it.setSurfaceProvider(viewFinder.createSurfaceProvider())
+                }
+
+            imageCapture = ImageCapture.Builder()
+                .build()
+
+            // Select back camera as a default
+            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+
+            try {
+                // Unbind use cases before rebinding
+                cameraProvider.unbindAll()
+
+                // Bind use cases to camera
+                cameraProvider.bindToLifecycle(
+                    this, cameraSelector, preview, imageCapture)
+
+            } catch(exc: Exception) {
+                Log.e(TAG, "Use case binding failed", exc)
+            }
+
+        }, ContextCompat.getMainExecutor(requireContext()))
+    }
+
+    private fun getOutputDirectory(): File {
+        val mediaDir = requireActivity().externalMediaDirs.firstOrNull()?.let {
+            File(it, resources.getString(R.string.app_name)).apply { mkdirs() } }
+        return if (mediaDir != null && mediaDir.exists())
+            mediaDir else requireActivity().filesDir
     }
 
     private fun setObservers() {
@@ -136,43 +229,31 @@ class HomeFragment : Fragment() {
         }
     }
 
-    private fun getPhotoFile(fileName: String): File {
-        //acess package specific directories
-        val storageDirectory =
-            requireActivity().getExternalFilesDir(Environment.DIRECTORY_PICTURES)
 
-        return File.createTempFile(fileName, ".jpg", storageDirectory)
-    }
-
-    private fun saveToFile(bitmap: Bitmap) {
-        val contextWrapper = ContextWrapper(requireActivity().applicationContext)
-        val directory = contextWrapper.getDir("imageDir", Context.MODE_PRIVATE)
-        val file = File(directory, System.currentTimeMillis().toString() + ".jpg")
-        if (!file.exists()) {
-            Log.d("path", file.toString())
-            var fos: FileOutputStream? = null
-            try {
-                fos = FileOutputStream(file)
-                bitmap.compress(Bitmap.CompressFormat.JPEG, 100, fos)
-                fos.flush()
-                fos.close()
-            } catch (e: IOException) {
-                e.printStackTrace()
+    override fun onRequestPermissionsResult(
+        requestCode: Int, permissions: Array<String>, grantResults:
+        IntArray) {
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+            if (allPermissionsGranted()) {
+                Toast.makeText(requireContext(),"Permissions Granted",Toast.LENGTH_LONG).show()
+                startCamera()
+            } else {
+                Toast.makeText(requireContext(),
+                    "Permissions not granted by the user.",
+                    Toast.LENGTH_SHORT).show()
+                requireActivity().finish()
             }
-        } else {
-            Log.d("path", "File Already Exists")
         }
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        if (requestCode == 1 && resultCode == Activity.RESULT_OK) {
-            val takenImage = BitmapFactory.decodeFile(photoFile.absolutePath)
-            saveToFile(takenImage)
-            val rotatedImage = ExifUtil.rotateBitmap(photoFile.absolutePath, takenImage)
-            imageView.setImageBitmap(rotatedImage)
-        } else {
-            super.onActivityResult(requestCode, resultCode, data)
-        }
+    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(
+            requireContext(), it) == PackageManager.PERMISSION_GRANTED
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        cameraExecutor.shutdown()
     }
 
     private fun logoutUser() {
@@ -254,5 +335,10 @@ class HomeFragment : Fragment() {
     companion object {
         @JvmStatic
         fun newInstance() = HomeFragment()
+
+        private const val TAG = "CameraXBasic"
+        private const val FILENAME_FORMAT = "yyyy-MM-dd-HH-mm-ss-SSS"
+        private const val REQUEST_CODE_PERMISSIONS = 10
+        private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
     }
 }
